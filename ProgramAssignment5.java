2982385ec39fce26b682a5bf39dff773b353219e
austinrockx <52753740+austinrockx@users.noreply.github.com>
2021-07-01 09:53:07 -0600

    Add files via upload

--- ProgramAssignment5.java
+++ ProgramAssignment5.java
@@ -3,7 +3,6 @@
 // June 29, 2021
 
 package csc160Assignments;
-import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -12,16 +11,14 @@ public class ProgramAssignment5 {
 	
 	//This cleans up the string by removing anything that is not a # or letter.
 	public String clean(String num) {
-		String regexClean = "[^a-zA-Z0-9_]";
-		Pattern clean = Pattern.compile(regexClean);
-		Matcher matcher = clean.matcher(num);
-		
-		String cleanNum = matcher.replaceAll("");;
-		return cleanNum;
+		return Pattern
+				.compile("[^a-zA-Z0-9_]")
+				.matcher(num)
+				.replaceAll("");
 	}
 	
 	//This checks the length of the phone number to verify if its 11.
-	public boolean minimumLength(String num) {
+	public boolean phoneNumberLength(String num) {
 		return num.length() == 11;
 	}
 	
@@ -32,105 +29,106 @@ public class ProgramAssignment5 {
 	
 	//This tests if the area code is numeric.
 	public boolean hasAreaCode(String num) {
-		try
-	    {
-			String subNum = num.substring(1, 3);
-	        Integer.parseInt(subNum);
+		try {
+	        Integer.parseInt(num.substring(1, 3));
 	        return true;
-	    } catch (NumberFormatException ex)
-	    {
+	    } catch (NumberFormatException ex) {
 	        return false;
 	    }
 	}
 	
 	//This tests if there is 7 digits after the area code.
 	public boolean hasSevenDigitPhoneNumber(String num) {
-		String subNum = num.substring(4,11);
+		String subNum = num.substring(4);
 		return subNum.length() == 7;
 	}
 	
 	//This compiles all the test methods to verify the number.
 	public boolean isVaildPhoneNumber(String num) {
 		
-		if(minimumLength(num) == false) {
-			System.out.println("Your phone number needs to be 11 digits");
-			return false;
+		boolean isValid = true;
+		
+		if(!phoneNumberLength(num)) {
+			System.out.println("Your phone number needs to be 11 digits.");
+			isValid = false;
 		}
 		
-		if(startsWithOne(num) == false) {
+		if(!startsWithOne(num)) {
 			System.out.println("You need to have a 1 and the start of the number.");
-			return false;
+			isValid = false;
 		}
 		
-		if(hasAreaCode(num) == false) {
+		if(!hasAreaCode(num)) {
 			System.out.println("You need an area code of 3 digits for your number.");
-			return false;
+			isValid = false;
 		}
 		
-		if(hasSevenDigitPhoneNumber(num) == false) {
-			System.out.println("You need to have 7 digits or characters after the area code");
-			return false;
+		if(!hasSevenDigitPhoneNumber(num)) {
+			System.out.println("You need to have 7 digits or characters after the area code.");
+			isValid = false;
 		}
 		
-		return true;
+		return isValid;
 	}
 	
 	//This converts the letters in the phone number into numbers.
 	public int getNumber(char ch) {
-		 switch(ch) {
-		 	case 'A': case 'B': case 'C': return 2;
-		 	case 'D': case 'E': case 'F': return 3;
-		 	case 'G': case 'H': case 'I': return 4;
-		 	case 'J': case 'K': case 'L': return 5;
-		 	case 'M': case 'N': case 'O': return 6;
-		 	case 'P': case 'Q': case 'R': case 'S': return 7;
-		 	case 'T': case 'U': case 'V': return 8;
-		 	case 'W': case 'X': case 'Y': case 'Z': return 9;
-		 }
-		 return -1;
+
+		if (Character.isDigit(ch)) {
+			return Character.getNumericValue(ch);
+		}
+		
+		switch(ch) {
+			case 'A': case 'B': case 'C': return 2;
+			case 'D': case 'E': case 'F': return 3;
+			case 'G': case 'H': case 'I': return 4;
+			case 'J': case 'K': case 'L': return 5;
+			case 'M': case 'N': case 'O': return 6;
+			case 'P': case 'Q': case 'R': case 'S': return 7;
+			case 'T': case 'U': case 'V': return 8;
+			case 'W': case 'X': case 'Y': case 'Z': return 9;
+		}
+		return -1;
 	}
 	
 	//This takes the inputed phone number and converts to the numeric phone number.
 	public String translateNumber(String num) {
-		String subNum = num.substring(4, 11);
-        char[] ch = new char[subNum.length()];
+
         String newPhoneNumber = num.substring(0, 4);
-        
-        for (int i = 0; i < subNum.length(); i++) {
-            ch[i] = subNum.charAt(i);
+
+        char[] ch = num.toCharArray();
+        for(int i = 4; i < ch.length; i++) {
+        	newPhoneNumber += getNumber(ch[i]);
         }
-        for(int i = 0; i < ch.length; i++) {
-        	if(Character.isDigit(ch[i])) {
-        		newPhoneNumber += ch[i];
-        	}
-        	if(Character.isLetter(ch[i])) {
-        		Character.toUpperCase(ch[i]);
-        		newPhoneNumber += getNumber(ch[i]);
-        	}
-        }
-        System.out.println(newPhoneNumber);
         
-		return null;
+		return newPhoneNumber;
 	}
 	
 	//This takes the numeric string and inputs the -'s. Extra Credit.
-	public String format() {
-		return null;
+	public String format(String num) {
+		return String.valueOf(num).replaceFirst("(\\d{1})(\\d{3})(\\d{3})(\\d+)", "$1-$2-$3-$4");
 	}
 	
 	public static void main(String[] args) {
 		ProgramAssignment5 pa5 = new ProgramAssignment5();
 		
 		Scanner scr = new Scanner(System.in);
-		
-		System.out.print("Enter a phone number string: ");
-		String phoneNumber = scr.next();
-		
-		pa5.clean(phoneNumber);
-		System.out.print(pa5.clean(phoneNumber) + "\n");
-		
-		pa5.isVaildPhoneNumber(pa5.clean(phoneNumber));
-		pa5.translateNumber(pa5.clean(phoneNumber));
+	
+		while(true) {
+			System.out.print("Enter a phone number string: ");
+			String phoneNumber = scr.next().toUpperCase();
+			
+			String cleanNumber = pa5.clean(phoneNumber);
+			
+			if (pa5.isVaildPhoneNumber(cleanNumber)) {
+				String newPhoneNumber = pa5.translateNumber(cleanNumber);
+		        System.out.println(pa5.format(newPhoneNumber) + "\n");
+		        scr.close();
+				System.exit(0);
+			}else {
+				System.out.println("Please try again. \n");
+				continue;
+			}
+		}
 	}
-
 }
